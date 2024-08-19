c8deae9686e558d1b4b544723dde85d107129e7c
Kontuzijus <nerius.rutkauskas@gmail.com>
2024-08-19 12:29:13 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cloudflare-bypasser',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'DrissionPage==4.0.5.6'
+    ],
+)
